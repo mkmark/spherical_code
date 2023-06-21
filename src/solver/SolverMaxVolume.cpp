8df210b0bc42d9c517e9956569472d8b85a771ce
mkmark <mark@mkmark.net>
2023-06-21 16:14:50 +0800

    feat: SolverMaxVolume

---
+++ src/solver/SolverMaxVolume.cpp
@@ -0,0 +1,35 @@
+#include <src/solver/SolverMaxVolume.hpp>
+
+
+int main(int argc, char* argv[])
+{
+  /// setting
+  std::cout.precision(std::numeric_limits<double>::max_digits10);
+
+  /// input
+  /// n
+  int n = std::stoi(argv[1]);
+  /// seed
+  srand48(std::stoi(argv[2]));
+  /// dump_base_path
+  std::string dump_base_path = "";
+  if (argc > 3) dump_base_path = argv[3];
+
+  /// run
+  /// solve
+  // auto begin = std::chrono::steady_clock::now();
+  auto solver_max_volume = SolverMaxVolume<double>(n, dump_base_path);
+  solver_max_volume.solve();
+  // auto end = std::chrono::steady_clock::now();
+  /// output
+  /// value
+  std::cout << solver_max_volume.value << std::endl;
+  /// step
+  std::cout << solver_max_volume.step << std::endl;
+  /// diff
+  std::cout << solver_max_volume.diff << std::endl;
+  /// time
+  // std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+
+  return 0;
+}
